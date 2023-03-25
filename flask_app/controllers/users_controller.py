bc454b9e67f1b3ced3d5000c9d8aa01cb938c612
Melissa Hernandez <mmdangelo11@gmail.com>
2023-03-25 09:23:21 -0700

    Start Cloudinary work

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -7,9 +7,14 @@ def home():
     return render_template("home.html")
 
 
+### ADMIN AND/OR HR USER ###
+# BADGES
+@app.route("/badge/new")
+def new_badge():
+    # need to add session check to protect route
+    return render_template("admin_new_badge.html")
 
-
-
+### STAFF USER ###
 # BADGES
 @app.route("/badges")
 def badges():
